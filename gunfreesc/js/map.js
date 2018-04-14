12184cf1b12a4e5379356e45c5e94b96428d8ea5
Suzanne Prentice <PrenticeS16@ashleyhall.org>
2018-04-14 16:27:52 -0400

    Directions

--- gunfreesc/js/map.js
+++ gunfreesc/js/map.js
@@ -3,7 +3,7 @@ var map;
 var currentPosition;
   function initMap() {
     geocoder = new google.maps.Geocoder();
-    
+
     var latlng = new google.maps.LatLng(-34.397, 150.644);
     var mapOptions = {
       zoom: 12,
@@ -13,7 +13,7 @@ var currentPosition;
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(showPosition);
     }
-    
+
     codeAddress(undefined, "Columbia, SC", undefined, false);
 
   }
@@ -35,25 +35,40 @@ var currentPosition;
       if (status == 'OK') {
         map.setCenter(results[0].geometry.location);
         if (mark) {
-          var contentString = '<h3>' + name + '</h3><h6>' + reason + '</h6>'
+          var posX = 33.9722359;
+          var posY = -81.01880970000002;
+          // if (!(currentPosition === 'undefined')) {
+          //   posX = currentPosition.lat();
+          //   posY = currentPosition.lng();
+          // }
+          var link = ' https://www.google.com/maps/dir/@'+
+          posX + ',' + posY + '/' +
+          encodeURI(address);
+
+          var dirString = '<a href="' + link + '">Get Directions</a>'
+          var contentString = '<h3>' + name + '</h3><h6>' + reason + '</h6>' + dirString;
           var infowindow = new google.maps.InfoWindow({
             content: contentString,
-
-          });  
+          });
           var marker = new google.maps.Marker({
             map: map,
             position: results[0].geometry.location,
             title: name,
         });
+        var close = true;
         marker.addListener('mouseover', function() {
           infowindow.open(map, marker);
         });
+        marker.addListener('click', function() {
+          close = false;
+        })
         marker.addListener('mouseout', function() {
-          infowindow.close();
+          if (close)
+            infowindow.close();
         })
 
         }
-        
+
       } else {
         alert('Geocode was not successful for the following reason: ' + status);
       }
