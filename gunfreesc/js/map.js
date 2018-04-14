213c2860a813003ecdbb12d20dbc903483f9f136
jrachele <jrachele@email.sc.edu>
2018-04-14 13:52:11 -0400

    map functionality

--- gunfreesc/js/map.js
+++ gunfreesc/js/map.js
@@ -1,17 +1,35 @@
 var geocoder;
-  var map;
+var map;
+var position;
   function initMap() {
     geocoder = new google.maps.Geocoder();
+    
     var latlng = new google.maps.LatLng(-34.397, 150.644);
     var mapOptions = {
       zoom: 8,
       center: latlng
     }
     map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+    }
+    // var marker = new google.maps.Marker({
+    //   map: map,
+    //   position: pos.coords,
+    // });
+
   }
 
-  function codeAddress() {
-    var address = document.getElementById('address').value;
+  function showPosition(position) {
+    var coordss = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    map.setCenter(coordss)
+    var marker = new google.maps.Marker({
+      map: map,
+      position: coordss,
+    });
+  }
+
+  function codeAddress(address) {
     geocoder.geocode( { 'address': address}, function(results, status) {
       if (status == 'OK') {
         map.setCenter(results[0].geometry.location);
