e416f5c176cce64cf4be9abf63d24bc04f709fbf
Ibrahim J <36282074+IntelligentBeard@users.noreply.github.com>
2018-04-14 13:26:31 -0400

    updated db.js

---
+++ gunfreesc/js/db.js
@@ -0,0 +1,27 @@
+(function(){
+
+  // Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyAmIDOWhsGzq_nqInVBovu8oG3srRCoO7c",
+    authDomain: "gunfreesodacity.firebaseapp.com",
+    databaseURL: "https://gunfreesodacity.firebaseio.com",
+    projectId: "gunfreesodacity",
+    storageBucket: "gunfreesodacity.appspot.com",
+    messagingSenderId: "696337298049"
+  };
+  firebase.initializeApp(config);
+
+  // Get elecments 
+  const preObject = document.getElementById('object');
+  //var place = {
+    //'name': "",
+    //'location': "",
+  //}
+
+  // create refrences 
+  const dbRefObject = firebase.database().databse().ref().chiled('object');
+
+  // realtime info
+  //dbRefObject.on('value', snap => console.log(snap.val())); 
+
+}());
