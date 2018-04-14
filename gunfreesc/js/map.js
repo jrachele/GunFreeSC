8d5866e2854788c6f3091d8524da2ec999160f35
Suzanne Prentice <PrenticeS16@ashleyhall.org>
2018-04-14 13:27:58 -0400

    New Map

--- gunfreesc/js/map.js
+++ gunfreesc/js/map.js
@@ -1,49 +1,26 @@
 var geocoder;
-var map;
-function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: uluru
-    });
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
-    });
+  var map;
+  function initMap() {
     geocoder = new google.maps.Geocoder();
-
+    var latlng = new google.maps.LatLng(-34.397, 150.644);
+    var mapOptions = {
+      zoom: 8,
+      center: latlng
+    }
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
   }
 
-  var getLocation =  function(address) {
-    var geocoder = new google.maps.Geocoder();
-    geocoder.geocode( { 'address': address}, function(results, status) {
-
-    if (status == google.maps.GeocoderStatus.OK) {
-        var latitude = results[0].geometry.location.lat();
-        var longitude = results[0].geometry.location.lng();
-        console.log(latitude, longitude);
-        return results[0].geometry.location;
-        }
-    });
-  }
-
-  //Call the function with address as parameter
-  getLocation('New York');
-
-/*
-function codeAddress(address) {
+  function codeAddress() {
+    var address = document.getElementById('address').value;
     geocoder.geocode( { 'address': address}, function(results, status) {
       if (status == 'OK') {
-          // map.setCenter(results[0].geometry.location);
-          // var marker = new google.maps.Marker({
-          //     map: map,
-          //     position: results[0].geometry.location
-        return results[0].geometry.location;
-        } else {
-          alert('Geocode was not successful for the following reason: ' + status);
-        }
-      });
-}
-initMap();
-console.log(codeAddress("208 Leyswood Dr. Greenville SC 29615"))
-*/
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+      } else {
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  }
